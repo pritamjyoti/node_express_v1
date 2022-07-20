695e33dcb3c7081203c24f9aba5c48dd6426bf3b
pritam13 <pritam@infiniteit.biz>
2022-07-20 19:20:14 +0530

    node uncoiught exception process

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 
 const app = require('./app/config/app');
+const process = require('process');
 require('dotenv').config();
-const PORT = process.env.PORT || 8081;
 
+const PORT = process.env.PORT || 8081;
+process.on('uncaughtException', (error, source) => {
+	console.log(source);
+ });
 var server = app.listen(PORT, function () {
 	var host = server.address().address
 	var port = server.address().port
